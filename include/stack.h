f84056dbfdff75ffacbeaf097b6f8bfaf586de20
Base <71930306+bshmanair@users.noreply.github.com>
2025-05-24 08:07:04 +0400

    something idk

--- include/stack.h
+++ include/stack.h
@@ -5,28 +5,28 @@
 
 typedef struct
 {
-    int* items;
+    int *items;
     int top;
     int capacity;
 } stack_t;
 
 // Constructor / Destructor
-stack_t* stack_create(int capacity);
-void stack_free(stack_t* stack);
+stack_t *stack_create(int capacity);
+void stack_free(stack_t *stack);
 
 // Core stack operations
-void stack_push(stack_t* stack, int value);
-int stack_pop(stack_t* stack);
-int stack_peek(stack_t* stack);
-void stack_shrink(stack_t* stack);
+void stack_push(stack_t *stack, int value);
+int stack_pop(stack_t *stack);
+int stack_peek(stack_t *stack);
+void stack_shrink(stack_t *stack);
 
-int stack_is_empty(stack_t* stack);
-int stack_is_full(stack_t* stack);
-int stack_size(stack_t* stack);
-int stack_capacity(stack_t* stack);
-void stack_print(stack_t* stack);
+int stack_is_empty(stack_t *stack);
+int stack_is_full(stack_t *stack);
+int stack_size(stack_t *stack);
+int stack_capacity(stack_t *stack);
+void stack_print(stack_t *stack);
 
 // Some optional operations I made
-stack_t* stack_create_from_array(int* arr, int length);
+stack_t *stack_create_from_array(int *arr, int length);
 
 #endif
